32eaa69a581111f8a95219e099f489c2eac08d48
Eyadoos <e.mhanna93@gmail.com>
2014-07-17 07:11:29 +0300

    add member

---
+++ Project2/Models/Class1.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Project2.Models
+{
+    public class Class1
+    {
+        public int i;
+        public int i2;
+    }
+}
